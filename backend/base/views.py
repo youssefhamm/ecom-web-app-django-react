d1b208547126941f15f4d90f42a0c463582e5f70
Youssef <hammoujan200p@gmail.com>
2023-10-30 20:07:05 +0100

    Create products and product details views and urls

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .products import products
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
 def getRoutes(request):
-    
-    return HttpResponse("hello")
+    return Response("hello")
 
 
+@api_view(['GET'])
 def getProducts(request):
-    return HttpResponse(products)
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for p in products:
+        if p["_id"] == pk:
+            product = p
+            break
+    return Response(product)
