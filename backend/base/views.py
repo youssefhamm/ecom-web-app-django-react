346da2d5482b284c7bed4a9427787d27729dbaa1
Youssef <hammoujan200p@gmail.com>
2023-10-30 19:38:02 +0100

    urls configuration

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .products import products
+
+def getRoutes(request):
+    
+    return HttpResponse("hello")
+
+
+def getProducts(request):
+    return HttpResponse(products)
