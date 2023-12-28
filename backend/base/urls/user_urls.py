a109838fec9731756867a590a4757904e4cc042c
Youssef <hammoujan200p@gmail.com>
2023-12-28 22:36:31 +0100

    Update profile EndPoint

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.getUsers, name="users"),
     path('login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register', views.registerUser, name='register'),
+    path('profile/update', views.updateUserProfile, name="profile-update"),
     path('profile', views.getUserProfile, name="profile"),
 
 ]
